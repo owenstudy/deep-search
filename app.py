9cacbb2d3a6186459bd2b1f8ba1ac71cd2bbf1a3
guangjun <user@UserdeMBP.COMFAST>
2023-04-19 23:25:48 +0800

    update

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    pid = os.getpid()
+    return f"Hello, World!{pid}"
 
 # app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
